517d894ba642c0ac41851777a39a585a89839db8
harip1087-wq <harip1087@gmail.com>
2025-08-21 15:45:34 +0530

    Add files via upload

---
+++ person.py
@@ -0,0 +1,8 @@
+class person:
+    def details(self,name,age):
+        print("Good morning Sir/Madam\nMy name is:",name)
+        print("My age is:",age)
+        print("what can i do for you Sir/Madam")
+myperson=person()
+myperson.details('alice',25)
+
