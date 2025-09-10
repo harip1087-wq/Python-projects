1f46e93304a99cb0d67d0362e0321147e28a91a6
harip1087-wq <harip1087@gmail.com>
2025-09-10 14:04:54 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,14 @@
+number1 = int(input("enter an number1:"))
+number2 = int(input(" enter an number2:"))
+print("1.add\n2.sub\n3.multiply\n4.divide")
+choice= int(input("enter your choice no:"))
+if(choice == 1):
+    print( number1+number2)
+elif(choice==2):
+    print( number1+number2)
+elif(choice==3):
+    print( number1*number2)
+elif(choice==4):
+    print( number1+number2)
+else:
+    print("invalid input")
